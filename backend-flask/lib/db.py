7150fca47d23e1f040d1de027921da4fe00d6a7b
Mitchell Puersten <mitch.puersten@gmail.com>
2023-03-24 20:56:23 +0000

    Successfully adding cruds and updating activities correctly.

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -8,8 +8,17 @@ class Db:
   def __init__(self):
     self.init_pool()
   
-  def template(self, name):
-    template_path = os.path.join(app.root_path, 'db', 'sql', name + '.sql')
+  def template(self, *args):
+    pathing = list((app.root_path, 'db', 'sql',) + args)
+    pathing[-1] = pathing[-1] + ".sql"
+    
+    template_path = os.path.join(*pathing)
+
+    green = '\033[92m'
+    no_color = '\033[0m'
+    print('\n')
+    print(f'{green}Load SQL Template ({template_path}) ---------------{no_color}')
+    
     with open(template_path, 'r') as f:
       template_content = f.read()
     return template_content
@@ -21,9 +30,15 @@ class Db:
   def print_sql(self, title, sql):
     cyan = '\033[96m'
     no_color = '\033[0m'
-    print('\n')
     print(f'{cyan}SQL statement ({title}) ---------------{no_color}')
     print(sql + '\n')
+
+  def print_params(self, params):
+    blue = '\033[94m'
+    no_color = '\033[0m'
+    print(f'{blue}SQL params:{no_color}')
+    for key, value in params.items():
+      print(key, ":", value)
 
   def query_commit(self, sql, params={}):
     self.print_sql("commit with returning", sql)
@@ -47,25 +62,29 @@ class Db:
       print ("ERROR******************************")
       self.print_sql_err(err)
 
-  def query_object_json(self, sql):
-    print("SQL statement (obj) ---------------")
-    print(sql + "\n")
+  def query_object_json(self, sql, params={}):
+    self.print_sql("array", sql)
+    
     wrapped_sql = self.query_wrap_object(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-        cur.execute(wrapped_sql)
+        cur.execute(wrapped_sql, params)
         json = cur.fetchone()
-        return json[0]
+        if json == None:
+          return None
+        else:
+          return json[0]
 
-  def query_array_json(self, sql):
-    print("SQL statement (array)---------------")
-    print(sql + "\n")
+  def query_array_json(self, sql, params={}):
+    self.print_sql("array", sql)
+    self.print_params(params)
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(wrapped_sql)
         json = cur.fetchone()
-        return json[0]
+        if json != None:
+          return json[0]
 
   def query_wrap_object(self, template):
     sql = f"""
