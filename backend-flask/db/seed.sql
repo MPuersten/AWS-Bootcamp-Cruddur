0b6a2042280001dbb7435e920475fd787df38715
Mitchell Puersten <mitch.puersten@gmail.com>
2023-04-01 17:45:20 +0000

    Implemented add new messages with issue in fetching existing user groups.

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   -- ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown', 'MOCK'),
-  -- ('Andrew Bayko', 'bayko@exampro.co', 'bayko', 'MOCK'),
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko', 'MOCK'),
   ('William Puersten', 'fake-second@gamil.com', 'WillP', 'MOCK'),
   ('Mitchell Puersten', 'fake-original@gmail.com', 'MitchP', 'MOCK');
 
