616b047697a2c7d017cef8f0d13273bc9a8cf6fd
Mitchell Puersten <mitch.puersten@gmail.com>
2023-04-19 16:30:09 +0000

    Adde example env vars, ts code for loading, and gitpod for loading.

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -8,12 +8,20 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     const bucketName = process.env.THUMBING_BUCKET_NAME!;
-    const functionPath = process.env.THUMBING_FUNCTION_PATH!;
     const folderInput = process.env.THUMBING_S3_FOLDER_INPUT!;
     const folderOutput = process.env.THUMBING_S3_FOLDER_OUTPUT!;
+    const webhookUrl = process.env.THUMBING_WEBHOOK_URL!;
+    const topicName = process.env.THUMBING_TOPIC_NAME!;
+    const functionPath = process.env.THUMBING_FUNCTION_PATH!;
+    console.log('bucketName',bucketName)
+    console.log('folderInput',folderInput)
+    console.log('folderOutput',folderOutput)
+    console.log('webhookUrl',webhookUrl)
+    console.log('topicName',topicName)
+    console.log('functionPath',functionPath)
 
     const bucket = this.createBucket(bucketName);
-    const lambda = this.createLambda(functionPath, folderInput, folderOutput);
+    const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
   }
 
   createBucket(bucketName: string): s3.Bucket {
