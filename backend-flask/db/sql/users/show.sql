a1dede9119adb735862fb1fbe49546fb1b34ec71
Mitchell Puersten <mitch.puersten@gmail.com>
2023-04-22 16:39:09 +0000

    Fixed image display error by making buckets public temporarily. Fixed misc issue in Profile Heading, user activites, and show.sql

--- backend-flask/db/sql/users/show.sql
+++ backend-flask/db/sql/users/show.sql
@@ -1,18 +1,18 @@
-SELECT
+SELECT 
   (SELECT COALESCE(row_to_json(object_row),'{}'::json) FROM (
     SELECT
       users.uuid,
       users.handle,
-      users.display_name
+      users.display_name,
       (
-        SELECT 
-          count(true)
-        FROM public.activities
-        WHERE 
-          activities.user_uuid = users.uuid
-      ) as cruds_count
+       SELECT 
+        count(true) 
+       FROM public.activities
+       WHERE
+        activities.user_uuid = users.uuid
+       ) as cruds_count
   ) object_row) as profile,
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM(
+  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
     SELECT
       activities.uuid,
       users.display_name,
@@ -23,9 +23,9 @@ SELECT
     FROM public.activities
     WHERE
       activities.user_uuid = users.uuid
-    ORDER BY activities.created_at DESC
+    ORDER BY activities.created_at DESC 
     LIMIT 40
   ) array_row) as activities
 FROM public.users
 WHERE
-  users.handle = %(handle)s  
+  users.handle = %(handle)s
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -4,33 +4,29 @@ from lib.db import db
 
 class UserActivities:
   def run(user_handle):
-    try:
-      # X-RAY -----
-      segment = xray_recorder.begin_segment('user_activities')
+    
+    model = {
+      'errors': None,
+      'data': None
+    }
 
-      model = {
-        'errors': None,
-        'data': None
-      }
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      print("else:")
+      sql = db.template('users','show')
+      results = db.query_object_json(sql,{'handle': user_handle})
+      model['data'] = results
 
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        sql = db.template('users', 'show')
-        results = db.query_object_json(sql, {'handle': user_handle})
-        model['data'] = results
+    # subsegment = xray_recorder.begin_subsegment('mock-data')
+    # # xray -----
+    # metadata = {
+    #   "now": now.isoformat(),
+    #   "results-size": len(model['data'])
+    # }
 
-      # subsegment = xray_recorder.begin_subsegment('mock-data')
-      # # xray -----
-      # metadata = {
-      #   "now": now.isoformat(),
-      #   "results-size": len(model['data'])
-      # }
-
-      # subsegment.put_metadata('now', metadata, 'services')
-
-    finally:
-      # xray_recorder.end_subsegment()
-      return model
+    # subsegment.put_metadata('now', metadata, 'services')
+    
+    return model
 
     
