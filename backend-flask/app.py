1a57e0abd9e5388034569a8cd4376bcb2ae85b0c
Mitchell Puersten <mitch.puersten@gmail.com>
2023-06-02 20:08:20 +0000

    Refactored backend routes.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask import request, after_this_request, g
-from flask_cors import CORS, cross_origin
 import os
 import sys
+
+from flask import Flask
+from flask import request, g
 
 from lib.rollbar import init_rollbar
 from lib.xray import init_xray
@@ -11,158 +11,21 @@ from lib.cors import init_cors
 from lib.cognito_jwt_token import jwt_required
 from lib.cloudwatch import init_cloudwatch
 
-from services.users_short import *
-from services.home_activities import *
-from services.notifications_activities import *
-from services.user_activities import *
-from services.create_activity import *
-from services.create_reply import *
-from services.search_activities import *
-from services.message_groups import *
-from services.messages import *
-from services.create_message import *
-from services.show_activity import *
-from services.update_profile import *
+import routes.activities
+import routes.general
+import routes.messages
+import routes.users
 
 app = Flask(__name__)
+routes.activities.load(app)
+routes.general.load(app)
+routes.messages.load(app)
 
 init_xray(app)
-with app.app_context():
-  init_rollbar(app)
-  
 init_honeycomb(app)
 init_cors(app)
-
-# @app.route('/rollbar/test')
-# def rollbar_test():
-#     rollbar.report_message('Hello World!', 'warning')
-#     return "Hello World!"
-
-def return_model(model):
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
-
-@app.route('/api/health-check')
-def health_check():
-  return {'success': True, 'ver': 2}, 200
-
-@app.route("/api/message_groups", methods=['GET'])
-@jwt_required()
-def data_message_groups():
-  model = MessageGroups.run(cognito_user_id=g.cognito_user_id)
-  return return_model(model)
-
-@app.route("/api/messages/<string:message_group_uuid>", methods=['GET'])
-@jwt_required()
-def data_messages(message_group_uuid):
-  model = Messages.run(
-    cognito_user_id=g.cognito_user_id,
-    message_group_uuid=message_group_uuid
-  )
-  return return_model(model)
-
-
-@app.route("/api/messages", methods=['POST','OPTIONS'])
-@cross_origin()
-@jwt_required()
-def data_create_message():
-  message_group_uuid   = request.json.get('message_group_uuid',None)
-  user_receiver_handle = request.json.get('handle',None)
-  message = request.json['message']
-
-  if message_group_uuid == None:
-    # Create for the first time
-    model = CreateMessage.run(
-      mode="create",
-      message=message,
-      cognito_user_id=g.cognito_user_id,
-      user_receiver_handle=user_receiver_handle
-    )
-  else:
-    # Push onto existing Message Group
-    model = CreateMessage.run(
-      mode="update",
-      message=message,
-      message_group_uuid=message_group_uuid,
-      cognito_user_id=g.cognito_user_id
-    )
-
-  return return_model(model)
-
-def default_home_feed(e):
-  # not authenticated
-  app.logger.debug(e)
-  app.logger.debug("unauthenticated")
-  data = HomeActivities().run()
-  return data, 200
-
-@app.route("/api/activities/home", methods=['GET'])
-@jwt_required(on_error=default_home_feed)
-# @xray_recorder.capture('activities_home')
-def data_home():
-  data = HomeActivities.run(cognito_user_id=g.cognito_user_id)
-  return data, 200
-
-@app.route("/api/activities/notifications", methods=['GET'])
-def data_notifications():
-  model = NotificationsActivities.run()
-  return return_model(model)
-
-@app.route("/api/activities/@<string:handle>", methods=['GET'])
-# @xray_recorder.capture('activities_users')
-def data_handle(handle):
-  model = UserActivities.run(handle)
-  return return_model(model)
-
-@app.route("/api/activities/search", methods=['GET'])
-def data_search():
-  term = request.args.get('term')
-  model = SearchActivities.run(term)
-  return return_model(model)
-
-@app.route("/api/activities", methods=['POST','OPTIONS'])
-@cross_origin()
-@jwt_required()
-def data_activities():
-  message = request.json['message']
-  ttl = request.json['ttl']
-  model = CreateActivity.run(message, g.cognito_user_id, ttl)
-  return return_model(model)
-
-@app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
-# @xray_recorder.capture('activities_show')
-def data_show_activity(activity_uuid):
-  data = ShowActivity.run(activity_uuid=activity_uuid)
-  return data, 200
-
-@app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
-@cross_origin()
-@jwt_required()
-def data_activities_reply(activity_uuid):
-  message = request.json['message']
-  ttl = request.json['ttl']
-  model = CreateReply.run(message, g.cognito_user_id, ttl)
-  return return_model(model)
-
-@app.route("/api/users/@<string:handle>/short", methods=['GET'])
-def data_users_short(handle):
-  data = UsersShort.run(handle)
-  return data, 200
+with app.app_context():
+  g.rollbar = init_rollbar(app)
 
 if __name__ == "__main__":
   app.run(debug=True)
-
-@app.route("/api/profile/update", methods=['POST','OPTIONS'])
-@cross_origin()
-@jwt_required()
-def data_update_profile():
-  bio          = request.json.get('bio',None)
-  display_name = request.json.get('display_name',None)
-  model = UpdateProfile.run(
-    cognito_user_id=g.cognito_user_id,
-    bio=bio,
-    display_name=display_name
-  )
-  return return_model(model)
