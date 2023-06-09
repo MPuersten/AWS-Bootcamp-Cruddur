50d2976f14ae5457add5317a54e3b77cfba20507
Mitchell Puersten <mitch.puersten@gmail.com>
2023-06-09 13:51:03 +0000

    Fixed FormError issues when errors not presented in array.

--- frontend-react-js/src/components/FormErrors.js
+++ frontend-react-js/src/components/FormErrors.js
@@ -5,11 +5,18 @@ export default function FormErrors(props) {
     let el_errors = null
 
     if (props.errors) {
-      el_errors = (<div className='errors'>
-        {props.errors.map(err_code => {
-          return <FormErrorItem err_code={err_code} />
-        })}
-      </div>)
+      if (Array.isArray(props.errors)) {
+        el_errors = (<div className='errors'>
+          {props.errors.map(err_code => {
+            return <FormErrorItem err_code={err_code} />
+          })}
+        </div>);
+      }
+      else {
+        el_errors = (<div className='errors'>
+          <FormErrorItem err_code={props.errors} />
+        </div>);
+      }
     }
 
     return (
